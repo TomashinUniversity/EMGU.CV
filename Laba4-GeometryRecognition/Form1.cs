85568201038b0769b1f1e6aa4587d560ec2a6f81
tomashined <tomashined@mail.ru>
2021-12-07 15:42:31 +0300

    start laba 4

---
+++ Laba4-GeometryRecognition/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace Laba4_GeometryRecognition
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void найтиФигурыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
