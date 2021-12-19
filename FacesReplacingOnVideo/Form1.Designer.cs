0974d601a7d0f29fa02ee96d564ec3190a6a338a
tomashined <tomashined@mail.ru>
2021-12-19 13:37:25 +0300

    FacesReplacingOnVideo

---
+++ FacesReplacingOnVideo/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace FacesReplacingOnVideo
+{
+    partial class FacesReplacingOnVideo
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.запуститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem,
+            this.запуститьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1605, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // запуститьToolStripMenuItem
+            // 
+            this.запуститьToolStripMenuItem.Name = "запуститьToolStripMenuItem";
+            this.запуститьToolStripMenuItem.Size = new System.Drawing.Size(91, 24);
+            this.запуститьToolStripMenuItem.Text = "Запустить";
+            this.запуститьToolStripMenuItem.Click += new System.EventHandler(this.запуститьToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1605, 745);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(796, 739);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(805, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(797, 739);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FacesReplacingOnVideo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1605, 773);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FacesReplacingOnVideo";
+            this.Text = "FacesReplacingOnVideo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem запуститьToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ FacesReplacingOnVideo/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.Util;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+
+namespace FacesReplacingOnVideo
+{
+    public partial class FacesReplacingOnVideo : Form
+    {
+        private static CascadeClassifier classifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+
+        private VideoCapture videoCapture = null; // Объект, содержащий в себе видео.
+        private double totalFrames = 0; // Общее количество кадров видео
+        private double currentFrame = 0; // Номер текущего кадра
+        private double fps = 0; // Текущий FPS видео
+        private bool play = false; // Состояние видео - воспроизведение/пауза
+
+        public FacesReplacingOnVideo()
+        {
+            InitializeComponent();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult dialogResult = openFileDialog1.ShowDialog(); // Открытие проводника
+
+                if (dialogResult == DialogResult.OK) // Если файл выбран
+                {
+                    videoCapture = new VideoCapture(openFileDialog1.FileName); // Установка видео
+                    Mat material = new Mat(); // Материал кадра
+                    videoCapture.Read(material); // Чтение первого кадра из видео в материал
+                    pictureBox1.Image = material.Bitmap; // Извлечение картинки из материала и установка в pictureBox
+
+                    totalFrames = videoCapture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameCount); // Количество кадров
+                    currentFrame = 1; // Установка первого кадра
+                    fps = videoCapture.GetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps); // FPS
+
+                }
+                else // Если не выбрали файл
+                {
+                    ShowErrorMessage("Видео не выбрано!");
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage(ex.Message);
+            }
+        }
+
+        private void запуститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (videoCapture == null)
+                    throw new Exception("Видео не выбрано");
+
+                play = true;
+                PlayVideo(true);
+            }
+            catch (Exception ex)
+            {
+                ShowErrorMessage(ex.Message);
+            }
+        }
+
+        private async void PlayVideo(bool withReplacing)
+        {
+            Mat m = new Mat();
+            while(play && currentFrame < totalFrames)
+                // Цикл будет идти пока активен показ и текущий кадр меньше общего количества
+            {
+                currentFrame++; // Включение следующего кадра
+                videoCapture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.PosFrames, currentFrame);
+                videoCapture.Read(m);
+                // Получение кадра по индексу из видео и извлечение из него картинки
+                pictureBox1.Image = m.Bitmap;
+                pictureBox2.Image = withReplacing ? ReplaceFaces(m.ToImage<Bgr, byte>()).Bitmap : m.Bitmap;
+                // Назначение оргинального кадра в первый pictureBox и изменение кадра во втором
+
+                await Task.Delay(100 / Convert.ToInt16(fps)); // Задержка в переключении кадров
+            }
+        }
+
+        private Image<Bgr, byte> ReplaceFaces(Image<Bgr, byte> image)
+        {
+            Rectangle[] faces = classifier.DetectMultiScale(image, 1.1, 0);
+            // Получение массива прямоугольников, содержащих лица
+            if (faces.Length > 1) // Если лиц больше 1, то делаем перестановку
+            {
+                for (int i = 0; i < faces.Length; i++)
+                {
+                    // Проход по всем лицам
+                    using (Graphics graphics = Graphics.FromImage(image.Bitmap))
+                    {
+                        int nextIndex = i + 1;
+                        if (nextIndex >= faces.Length) nextIndex = 0;
+                        // Получение индекса следующего лица
+
+                        Image newFace = CropImage(image.Bitmap, faces[nextIndex]);
+                        // Получение картинки следующего лица по индексу
+
+                        graphics.DrawImage(newFace, faces[i]);
+                        // Замена старого лица новым поверх
+                    }
+                }
+            }
+            return image;
+        }
+
+        private Bitmap CropImage(Bitmap source, Rectangle section)
+        {
+            // Вырезание картинки по прямоугольнику с оригинала
+            var bitmap = new Bitmap(section.Width, section.Height);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+                return bitmap;
+            }
+        }
+
+        private void ShowErrorMessage(string error)
+        {
+            MessageBox.Show(error, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
